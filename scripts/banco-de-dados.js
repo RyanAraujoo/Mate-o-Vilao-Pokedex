ebb50938ee1ba99a0d179025afffa7f21576eb09
Ryan Pablo Silva Araujo <zryanaraujoo@gmail.com>
2022-05-29 14:10:21 -0300

    atualizacao aplicação - estruturação

---
+++ scripts/banco-de-dados.js
@@ -0,0 +1,54 @@
+const infoPokers = [
+    {
+        nome: "default",
+        src: "../assets/pokers/pokemon.jpg"
+    },
+    {
+        nome: "Charizard",
+        ataque: 140,   
+        vida: 210,
+        src: "../assets/pokers/charizard-pokemon-charizard.gif"    
+    },
+    {
+        nome: "Gengar",
+        ataque: 75,   
+        vida: 180,
+        src: "../assets/pokers/pokemon-gengar.gif"
+    },
+    {
+        nome: "Bulbasaur",
+        ataque: 100,   
+        vida:  200,
+        src: "../assets/pokers/bulbasaur.gif"       
+    },
+    {
+        nome:  "Umbreon",
+        ataque: 90,   
+        vida: 150,
+        src: "../assets/pokers/umbreon-pokemon.gif"       
+    },
+    {
+        nome: "Lucario",
+        ataque: 120,   
+        vida: 170,      
+        src: "../assets/pokers/lucario-pokemon.gif"
+    },
+    {
+        nome: "Gardevoir",
+        ataque: 80,   
+        vida: 190,
+        src: "../assets/pokers/gardevoir-pokemon.gif" 
+    },
+    {
+        nome: "Eevee",
+        ataque: 50,   
+        vida: 150,
+        src: "../assets/pokers/eevee-pokemon.gif"
+    },
+    {
+        nome: "Pikachu",
+        ataque: 100,   
+        vida: 200,
+        src: "../assets/pokers/pokemon-pikachu.gif"          
+    }
+]
